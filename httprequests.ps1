fb7858417aa60f00fc616a6699008020c39354ae
Isa Dag <isadag@users.noreply.github.com>
2024-05-23 09:42:25 +0200

    Update httprequests.ps1

--- httprequests.ps1
+++ httprequests.ps1
@@ -1,3 +1,4 @@
+# For powershell 5.1
 param (
     [Parameter(Mandatory=$true)]
     [string]$Url,
@@ -11,8 +12,29 @@ for ($i = 1; $i -le $Count; $i++) {
     $results += Start-Job -ScriptBlock {
         param($Url)
 
-        # Dot-source the script to make sure the function is available
-        . $PSScriptRoot\ParallelHttpRequests.ps1
+        # Function to perform an HTTP request and return the response time, status code, and error message
+        function Test-HttpRequest {
+            param (
+                [string]$Url
+            )
+
+            try {
+                $response = Invoke-WebRequest -Uri $Url -Method Get -TimeoutSec 30 -ErrorAction Stop
+                $statusCode = $response.StatusCode
+                $responseTime = $response.Headers['X-Response-Time']
+                $errorMessage = $null
+            } catch {
+                $statusCode = $_.Exception.Response.StatusCode.Value__
+                $responseTime = 0
+                $errorMessage = $_.Exception.Message
+            }
+
+            return @{
+                StatusCode = $statusCode
+                ResponseTime = $responseTime
+                ErrorMessage = $errorMessage
+            }
+        }
 
         # Call the function within the job
         Test-HttpRequest -Url $Url
